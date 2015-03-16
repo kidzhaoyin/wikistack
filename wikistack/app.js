3f4cc6950779cb5974a27b7607979ebdfb1b5347
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-16 02:51:32 -0400

    added add module

--- wikistack/app.js
+++ wikistack/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var swig = require('swig');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,12 +8,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var add_routes = require('./routes/add');
 
 var app = express();
+app.engine('html', swig.renderFile);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/add', add_routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -37,6 +40,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  swig.setDefaults({ cache: false });
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
