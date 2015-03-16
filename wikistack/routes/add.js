4e0e3a4ef48061a96c6488b373beafe58605ab5a
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-16 03:23:31 -0400

    submitting form

--- wikistack/routes/add.js
+++ wikistack/routes/add.js
@@ -11,6 +11,24 @@ router.get('/', function(req, res) {
 });
 
 router.post('/submit', function(req, res) {
+    var models = require('../models/');
+    var title = req.body.title;
+    var body = req.body.content;
+    var url_name = new generateUrlName(req.body.title);
+    var p = new models.Page({"title": title, "body": body, "url_name":url_name});
+    p.save();
+    res.redirect('/');
 });
+
+var generateUrlName = function(name) {
+  if (typeof name != "undefined" && name !== "") {
+    // Removes all non-alphanumeric characters from name
+    // And make spaces underscore
+    return name.replace(/\s/ig,"_").replace(/\W/ig,"");
+  } else {
+    // Generates random 5 letter string
+    return Math.random().toString(36).substring(2,7);
+  }
+};
 
 module.exports = router;
