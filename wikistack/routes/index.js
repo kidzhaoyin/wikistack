abb8352194ad3cc7519012a8521ad9e9e53fee0f
Yin Zhao <kidzhaoyin@gmail.com>
2015-03-16 11:07:27 -0400

    added about_us and wiki title to homepage

--- wikistack/routes/index.js
+++ wikistack/routes/index.js
@@ -9,4 +9,16 @@ router.get('/', function(req, res, next) {
    });
 });
 
+router.get('/wiki/:title', function(req,res,next){
+   var title = req.params.title.replace(/_/g,' ');
+   model.Page.findOne({ 'title':title}, 'title body', function(err,data){
+	res.render('show',{page:data});
+   });
+
+});
+
+router.get('/about_us',function(req,res,next){
+   res.render('about_us');
+});
+
 module.exports = router;
